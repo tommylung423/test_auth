89a8bef3a3535d0f0e2caa27e998d1b2acde4fd7
tommylung423 <64849149+tommylung423@users.noreply.github.com>
2021-05-21 01:40:53 +0800

    +body-parser

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 var cors = require('cors')
 const mongoose = require('mongoose');
+const bp = require("body-parser");
 
-app.use(cors()) 
+app.use(cors());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,9 @@ router.get('/me', auth, async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
+  if (error) console.log(error.details[0].message);
   if (error) return res.status(400).send(error.details[0].message);
+
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
