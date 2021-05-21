77ebde5c6ec63eac531074bc8c88abf3246e111d
tommylung423 <64849149+tommylung423@users.noreply.github.com>
2021-05-21 20:30:03 +0800

    change db link

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async function() {
-  await mongoose.connect('mongodb+srv://user:tommy@cluster0.pu2qb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  await mongoose.connect('mongodb+srv://dbuser:XKKqs32MLP5Xw*X@cluster0.d0fpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(() => console.log('Connected to MongoDB...'));
 }
 
